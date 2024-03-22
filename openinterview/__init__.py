d6c923e01386066895bfba7c0cc5c348e13974b7
dsdanielpark <parkminwoo1991@gmail.com>
2024-03-22 19:30:08 +0900

    build: 1.0.11 for google colab env

--- openinterview/__init__.py
+++ openinterview/__init__.py
@@ -1,10 +1,13 @@
-from .manager import InterviewManager
-from .models.claude import InterviewClaude
 from .models.gpt import InterviewGPT
-from .modules.voice.random_play import RandomPlayer
+from .models.claude import InterviewClaude
+
+from .manager import InterviewManager
+
 from .utils.doc_manager import DocumentCreator
 from .utils.file_manager import load_file_content
 from .utils.prompter import create_system_prompt, create_base_prompt
+
+from .modules.voice.random_play import RandomPlayer
 from .modules.voice.google import google_tts, google_stt, save_google_tts
 
 try:
@@ -13,5 +16,5 @@ except ImportError:
     pass
 
 
-__version__ = "1.0.10"
+__version__ = "1.0.11"
 __author__ = "daniel park <parkminwoo1991@gmail.com>"
